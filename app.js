3ff0b3310d6544e86d5b1eaf8732fa0c6e5f3afb
Ajay <ajay.raj@roadelabs.com>
2024-09-09 12:05:30 +0530

    Added files

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+// setup cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header(
+            'Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET'
+        );
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// Enable All CORS Requests
+app.use(cors());
+
+// use morgan logger
+app.use(
+    morgan(
+        ':method :url :status :res[content-length] - :response-time ms - :date[web]'
+    )
+);
+
+// set bodyParser
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+app.use(bodyParser.json());
+
+app.use('/profile', express.static('public/profile'));
+
+app.use('/api', routes);
+
+// 404 not found
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    });
+});
+
+//define routes here
+
+
+// Use the imported routes
+// app.use('/api', routes);
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+require('dotenv').config();
+const app = require('./app');
+
+const port = process.env.PORT || 7070;
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log('Server Running on port ' + port);
